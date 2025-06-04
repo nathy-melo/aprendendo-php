f7168669086fde0a367bf4150121a98774b98fc5
nathy-melo <natymelo2008@gmail.com>
2025-06-04 19:15:36 -0300

    again nt

--- Questao7.php
+++ Questao7.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercícios PHP</title>
-    <link rel="stylesheet" href="Atividade 1.css">
+    <link rel="stylesheet" href="GeralStyle.css">
 
     <?php
     function Biblioteca($Livros, $Usuario, $Nome, $Codigo_do_Livro)
